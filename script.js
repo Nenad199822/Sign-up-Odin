cd46fb6560df83a147fa8fa43f8a14717032e208
Nenad199822 <neso.zabjelo@gmail.com>
2023-10-10 17:21:42 +0200

    Style button and make box shadow on form thats it

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var password = document.getElementById('pwd1');
-var passwordConfirm = document.getElementById('pwd')
-if (password.value != passwordConfirm.value) {
-    passwordConfirm.innerHTML('Password does not match')
+function validatePassword() {
+    var password = document.getElementById('pwd1').value;
+    var confirmPassword = document.getElementById('confirm-pwd').value
+    if (password !== confirmPassword) {
+        alert('Passwords does not match')
+        return false
+    }
+    return true;
 }
+validatePassword();
